0347c5b3716d0c2796e6612c14d6b0cc79bc5775
Xakiru <xakiru@gmail.com>
2023-05-20 02:09:46 +0100

    Update magenta.py

--- scripts/magenta.py
+++ scripts/magenta.py
@@ -208,7 +208,7 @@ class ScriptPostprocessingUpscale(scripts_postprocessing.ScriptPostprocessing):
         #output = Image.fromarray(cv2.merge((r, g, b, a)))
         output=cv2.cvtColor(output, cv2.COLOR_RGBA2RGB)
         if (save_magenta):
-            images.save_image(output,basename= "magenta_" ,path=opts.outdir_save,  extension=opts.samples_format, info= pp.info) 
+            images.save_image(Image.fromarray(output),basename= "magenta_" ,path=opts.outdir_save,  extension=opts.samples_format, info= pp.info) 
 
         if (forward_magenta):
             pp.image=output 
