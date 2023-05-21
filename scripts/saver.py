b37fe8abc4837d9c167684c7beaa386c194f6c72
Xakiru <xakiru@gmail.com>
2023-05-21 16:41:18 +0100

    Update saver.py

--- scripts/saver.py
+++ scripts/saver.py
@@ -22,8 +22,5 @@ class ScriptPostprocessingAtlas(scripts_postprocessing.ScriptPostprocessing):
     def process(self, pp: scripts_postprocessing.PostprocessedImage, enable):
         if not enable:
             return
-        for item in pp.info:
-            print(item)
-        print(pp.info["seed"])
-        print(pp.info)
-        images.save_image(pp.image,basename= str("aa")+"-original" ,path=opts.outdir_img2img_samples,  extension=opts.samples_format, info= pp.info) 
+
+        images.save_image(pp.image,basename= "original" ,path=opts.outdir_img2img_samples,  extension=opts.samples_format, info= pp.info) 
